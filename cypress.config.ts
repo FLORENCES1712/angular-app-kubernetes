6986a6aa2be1f17c202d6ac984a99f805da2fa84
FLORENCES1712 <101697175+FLORENCES1712@users.noreply.github.com>
2023-03-31 13:22:02 +0530

    Update cypress.config.ts

--- cypress.config.ts
+++ cypress.config.ts
@@ -16,14 +16,7 @@ async function setupNodeEvents(
           extensions: ['.ts', '.js'],
         },
         module: {
-          rules: [
-            {
-              test: /\.ts$/,
-              exclude: [/node_modules/],
-              use: [
-                {
-                  loader: 'ts-loader',
-                },
+  
               ],
             },
             {
